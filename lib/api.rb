6824e99dfa490cc50f095cdff518753313717499
root <root@ubuntu.(none)>
2014-02-09 13:34:50 +0800

    api.rb中冗余代码的删除

--- lib/api.rb
+++ lib/api.rb
@@ -3,21 +3,21 @@ require "sinatra/base"
 # API模块基于Sinatra/Base类，通过register方式将各种不同的业务controller文件组织起来
 module Api
 
-    module RequireUser
-        def self.registered(app)
-            app.before do
-                # Rails.logger.info "Sinatra path - #{request.path_info} : #{params.inspect}"
-                unless request.path_info.match(not_require_routes_reg)
-                    require_user
-                end
-            end
+    # module RequireUser
+    #     def self.registered(app)
+    #         app.before do
+    #             # Rails.logger.info "Sinatra path - #{request.path_info} : #{params.inspect}"
+    #             unless request.path_info.match(not_require_routes_reg)
+    #                 require_user
+    #             end
+    #         end
 
-            app.helpers Helpers
-        end
+    #         app.helpers Helpers
+    #     end
 
-        module Helpers
-        end
-    end
+    #     module Helpers
+    #     end
+    # end
 
     module V1
         class Server < Sinatra::Base
